1a9ba38e4d95216013ae80f19ad67e054ccf83e2
Juan <juancm86@gmail.com>
2020-06-05 16:48:39 -0300

    - Limpando projeto web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <h1>Olá aa</h1>
-      </header>
-    </div>
+    <h1>Olá aa</h1>
   );
 }
 
