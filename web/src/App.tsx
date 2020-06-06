3cb2953cf1a1064be82152e9c72896cb2f73987f
Juan <juancm86@gmail.com>
2020-06-06 11:15:29 -0300

    - conceitos react

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import Header from './Header'; 
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick () {
+    setCounter(counter + 1);
+  }
+
   return (
-    <h1>Olá aa</h1>
+    <div>
+      <Header title={`Contador: ${counter}`} />
+
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+    </div>
   );
 }
 
